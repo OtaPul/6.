0ebb7d08daa396f1c1d109682e0c87e3a7e650da
OtaPul <otto.pulkkinen@student.lut.fi>
2026-02-16 22:57:44 +0200

    Add Instrument class with manufacturer and price

---
+++ Instrument.java
@@ -0,0 +1,14 @@
+public class Instrument {
+
+    protected String manufacturer;
+    protected int price;
+
+    public Instrument(String manufacturer, int price) {
+        this.manufacturer = manufacturer;
+        this.price = price;
+    }
+
+    public String getDetails() {
+        return "Valmistaja: " + manufacturer + ", Hinta: " + price + "e";
+    }
+}
