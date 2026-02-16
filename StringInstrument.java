d852c26f5724ae1af178596961f355c5bc027491
OtaPul <otto.pulkkinen@student.lut.fi>
2026-02-16 22:58:05 +0200

    Add StringInstrument class with tuning method

---
+++ StringInstrument.java
@@ -0,0 +1,13 @@
+public class StringInstrument extends Instrument {
+
+    protected int numberOfStrings;
+
+    public StringInstrument(String manufacturer, int price, int numberOfStrings) {
+        super(manufacturer, price);
+        this.numberOfStrings = numberOfStrings;
+    }
+
+    public void tune() {
+        System.out.println(manufacturer + " soittimesta viritettiin " + numberOfStrings + " kieltä!");
+    }
+}
